41d80cc41d1267b7bd88167c81a94317d5f3b647
Yassine JOUT <yassinejout@gmail.com>
2020-03-08 14:18:42 +0100

    refactor(apps): - remove unused files. - folder structure.

--- diboo-restaurant/src/App.tsx
+++ diboo-restaurant/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Row, Column } from './Style/containers.style';
-import Home from './Pages/Home';
 
 const App: React.FC = () => {
   return (
     <div className="page">
-       <Home/>
+      Diboo Restaurant Admin app
     </div>
   );
 }
