859fe05be09a61bcc76c11ab5eaef40218febdeb
Yassine JOUT <yassinejout@gmail.com>
2020-03-09 20:51:04 +0100

    refactor(dibooAdmin): install react router dom / history

--- diboo-admin/src/App.tsx
+++ diboo-admin/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from './pages/Login';
-
+import { Router,Route } from 'react-router-dom';
 const App: React.FC = () => {
   return (
     <div className="App">
