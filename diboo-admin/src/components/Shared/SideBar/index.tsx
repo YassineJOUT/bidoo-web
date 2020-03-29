c6c6409a950142e182ae46a05bd1609033ea566b
Yassine JOUT <yassinejout@gmail.com>
2020-03-29 18:23:13 +0100

    refactor(sideBar): house keeping

--- diboo-admin/src/components/Shared/SideBar/index.tsx
+++ diboo-admin/src/components/Shared/SideBar/index.tsx
@@ -1,141 +1,124 @@
 import React, { Component } from "react";
-import { history } from '../../../utilities/history'
-import { Router,Route,Link } from 'react-router-dom';
-import MetisMenu from 'react-metismenu';
+import { history } from "../../../utilities/history";
+import { Router, Route, Link } from "react-router-dom";
+import MetisMenu from "react-metismenu";
 class SideBar extends Component {
- 
-    //history.push("forgotpassword")
-    
+  //history.push("forgotpassword")
+
   render = () => {
-    const content=[
-        {
-            icon: 'ion ion-md-speedometer',
-            label: 'Dashboard',
-            to: '/dashboard',
-        },
-        {
-            icon: 'ion ion-md-pin',
-            label: 'Restaurant Map View',
-            to: '/map-view',
-        },
-        {
-            icon: 'ion ion-md-home',
-            label: 'Settings',
-            content: [
-                {
-                   
-                    label: 'Site settings',
-                    to: '/site-settings',
-                },
-                {
-                   
-                    label: 'Commission settings',
-                    to: '/commission-settings',
-                },
-            ],
-        },
-        {
-            icon: 'ion ion-md-business',
-            label: 'Manage restaurants',
-            content: [
-                {
-                    label: 'Add restaurant',
-                    to: '/add-restaurant',
-                },
-                {
-                    label: 'Manage restaurant',
-                    to: '/manage-restaurants',
-                },
-                {
-                    label: 'Orders Report',
-                    to: '/restaurant-report',
-                },
-            ],
-        }, 
-        {
-            icon: 'ion ion-md-business',
-            label: 'Menu management',
-            content: [
-                {
-                    label: 'Kitchen',
-                    to: '/kitchen',
-                },
-                {
-                    label: 'Menu categories',
-                    to: '/menu-category',
-                },
-                {
-                    label: 'Category items',
-                    to: '/tems',
-                },
-            ],
-        }, 
-        {
-            icon: 'ion ion-md-restaurant',
-            label: 'Manage customers',
-            content: [
-                {
-                   
-                    label: 'Customers',
-                    to: '/customer',
-                },
-                {
-                   
-                    label: 'Track payment',
-                    to: '/track-payment',
-                },
-            ],
-        },
-        {
-            icon: 'ion ion-md-cart',
-            label: 'Manage orders',
-            content: [
-                {
-                   
-                    label: 'Customer orders',
-                    to: '/orders',
-                },
-                {
-                   
-                    label: 'Guest orders',
-                    to: '/orders-guest',
-                },
-            ],
-        }, 
+    const content = [
+      {
+        icon: "ion ion-md-speedometer",
+        label: "Dashboard",
+        to: "/dashboard"
+      },
+      {
+        icon: "ion ion-md-pin",
+        label: "Restaurant Map View",
+        to: "/map-view"
+      },
+      {
+        icon: "ion ion-md-home",
+        label: "Settings",
+        content: [
+          {
+            label: "Site settings",
+            to: "/site-settings"
+          },
+          {
+            label: "Commission settings",
+            to: "/commission-settings"
+          }
+        ]
+      },
+      {
+        icon: "ion ion-md-business",
+        label: "Manage restaurants",
+        content: [
+          {
+            label: "Add restaurant",
+            to: "/add-restaurant"
+          },
+          {
+            label: "Manage restaurant",
+            to: "/manage-restaurants"
+          },
+          {
+            label: "Orders Report",
+            to: "/restaurant-report"
+          }
+        ]
+      },
+      {
+        icon: "ion ion-md-business",
+        label: "Menu management",
+        content: [
+          {
+            label: "Kitchen",
+            to: "/kitchen"
+          },
+          {
+            label: "Menu categories",
+            to: "/menu-category"
+          },
+          {
+            label: "Category items",
+            to: "/tems"
+          }
+        ]
+      },
+      {
+        icon: "ion ion-md-restaurant",
+        label: "Manage customers",
+        content: [
+          {
+            label: "Customers",
+            to: "/customer"
+          },
+          {
+            label: "Track payment",
+            to: "/track-payment"
+          }
+        ]
+      },
+      {
+        icon: "ion ion-md-cart",
+        label: "Manage orders",
+        content: [
+          {
+            label: "Customer orders",
+            to: "/orders"
+          },
+          {
+            label: "Guest orders",
+            to: "/orders-guest"
+          }
+        ]
+      }
     ];
-   
+
     return (
-        <div className="left side-menu">
-        <div className="slimScrollDiv " ><div className="slimscroll-menu mm-show" id="remove-scroll" >
-
-        
+      <div className="left side-menu">
+        <div className="slimScrollDiv ">
+          <div className="slimscroll-menu mm-show" id="remove-scroll">
             <div id="sidebar-menu" className="mm-active">
-              
-            <MetisMenu className="sidebar-menu"
-                        classNameContainer="metismenu"
-                        classNameItemActive="mm-active"
-                        classNameLinkActive="mm-active"
-                content={content} activeLinkFromLocation />
+              <MetisMenu
+                className="sidebar-menu"
+                classNameContainer="metismenu"
+                classNameItemActive="mm-active"
+                classNameLinkActive="mm-active"
+                content={content}
+                activeLinkFromLocation
+              />
             </div>
-           
+
             <div className="clearfix"></div>
-
-        </div><div></div></div>
-      
-
-    </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     );
   };
 }
 
-/*const mapStateToProps = ({ login }: ApplicationState) => ({
-  userInfo: login.userInfo,
-  isLoggedIn: login.isLoggedIn,
-  error: login.error,
-  isLoading: login.isLoading
-});*/
-
-//const mapActionsToProps = { login };
-
-//export default connect(mapStateToProps, mapActionsToProps)(LoginPage);
 export default SideBar;
