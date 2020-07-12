12c35ec888e053d30564dbac7952a4cf90ebdefe
Yassine JOUT <yassinejout@gmail.com>
2020-07-12 20:05:45 +0100

    feat(comission): edit over all commission

--- diboo-admin/src/components/Settings/Commission/Forms/index.tsx
+++ diboo-admin/src/components/Settings/Commission/Forms/index.tsx
@@ -1,34 +1,107 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
+import { Formik, Field } from "formik";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import {
+  EDIT_COMMISSION_MUTATION,
+  GET_COMMISSION_SETTING_MUTATION,
+} from "../../../../helpers/gql";
+import { printIntrospectionSchema } from "graphql";
+import Alert from "../../../Shared/Alert";
 
-class CommissionForm extends Component {
-  render = () => {
-    return (
-      <form>
-        <div className="row">
-          <div className="col-lg-6">
-            <h4 className="mt-0 header-title">Commission</h4>
-            <p className="text-muted mb-4">Fill all information below</p>
-
-            <div className="form-group">
-              <label>Commission %</label>
-              <input
-                id="metakeywords"
-                name="tax"
-                type="text"
-                className="form-control"
-              />
-            </div>
-            <button
-              type="submit"
-              className="btn btn-success mr-1 waves-effect waves-light"
-            >
-              Save Changes
-            </button>
-          </div>
-        </div>
-      </form>
-    );
-  };
+interface commissionProps {
+  id: string;
+  commission: Number;
 }
+
+const CommissionForm: React.FunctionComponent = (props) => {
+  const [showAlert, setShowAlert] = useState({
+    show: false,
+    message: "",
+    type: true,
+  });
+  const handlAlert = (show: boolean, message: string, type: boolean) => {
+    setShowAlert({ show, message, type });
+    setTimeout(() => {
+      setShowAlert({ show: false, message: "", type: true });
+    }, 2000);
+    refetch();
+  };
+  const [comVals, setComVals] = useState({
+    id: "",
+    commission: 0,
+  });
+  const editCommission = (values: commissionProps, actions: any) => {
+    editCommissionMutation({ variables: values });
+  };
+  const [
+    editCommissionMutation,
+    { loading: mutationLoading, error: mutationError },
+  ] = useMutation(EDIT_COMMISSION_MUTATION, {
+    onCompleted: (data) => {
+      const { ok, message, error } = data.updateCommission;
+      handlAlert(true, ok ? message : error, ok);
+      refetch();
+    },
+    onError: (err) => console.log(err),
+  });
+
+  const { refetch, loading } = useQuery(GET_COMMISSION_SETTING_MUTATION, {
+    onCompleted: (data) => {
+      console.log(data);
+      data.getCommission.ok &&
+        setComVals({
+          id: data.getCommission.data.id,
+          commission: data.getCommission.data.commission,
+        });
+    },
+  });
+  return (
+    <Formik
+      enableReinitialize
+      initialValues={comVals}
+      // validationSchema={loginValidationSchema}
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        return editCommission(values, { setSubmitting, resetForm });
+      }}
+    >
+      {({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-lg-6">
+              <h4 className="mt-0 header-title">Commission</h4>
+              <p className="text-muted mb-4">Fill all information below</p>
+              {!loading ? (
+                <div>
+                  <div className="form-group">
+                    <label>Commission %</label>
+                    <Field
+                      type="number"
+                      className="form-control"
+                      name="commission"
+                      placeholder="Commission"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-success mr-1 waves-effect waves-light"
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              ) : (
+                <div className="d-flex justify-content-center">
+                  <div className="spinner-border "></div>
+                </div>
+              )}
+            </div>
+          </div>
+          {showAlert.show && (
+            <Alert message={showAlert.message} type={showAlert.type} />
+          )}
+        </form>
+      )}
+    </Formik>
+  );
+};
 
 export default CommissionForm;
--- diboo-admin/src/helpers/gql.ts
+++ diboo-admin/src/helpers/gql.ts
@@ -1,76 +1,111 @@
 import { gql } from "apollo-boost";
 
 export const USER_LOGIN_MUTATION = gql`
-mutation login($email: String!,$password: String!) {
-    login(input:{email: $email,password: $password}){
-        ok
-        error
-        data{
-            id
-        }
+  mutation login($email: String!, $password: String!) {
+    login(input: { email: $email, password: $password }) {
+      ok
+      error
+      data {
+        id
+      }
     }
   }
 `;
 
 export const ADD_CAROUSEL_MUTATION = gql`
-mutation createCarousel($id: String,$title: String,$subtitle: String, $bannerLink: String,$image: Upload) {
-    createCarousel(input:{id: $id,title: $title,subtitle: $subtitle, bannerLink: $bannerLink,image:$image}){
-        ok,
-        message,
-        error
+  mutation createCarousel(
+    $id: String
+    $title: String
+    $subtitle: String
+    $bannerLink: String
+    $image: Upload
+  ) {
+    createCarousel(
+      input: {
+        id: $id
+        title: $title
+        subtitle: $subtitle
+        bannerLink: $bannerLink
+        image: $image
+      }
+    ) {
+      ok
+      message
+      error
+    }
+  }
+`;
+export const EDIT_COMMISSION_MUTATION = gql`
+  mutation updateCommission($id: ID, $commission: Float) {
+    updateCommission(input: { id: $id, commission: $commission }) {
+      ok
+      message
+      error
     }
   }
 `;
 export const EDIT_CAROUSEL_STATUS_MUTATION = gql`
-mutation editStatusCarousel($id: String,$status: Boolean) {
-    editStatusCarousel(input:{id: $id,status: $status}){
-        ok,
-        message,
-        error
+  mutation editStatusCarousel($id: String, $status: Boolean) {
+    editStatusCarousel(input: { id: $id, status: $status }) {
+      ok
+      message
+      error
     }
   }
 `;
 export const DELETE_MUTATION_MUTATION = gql`
-mutation deleteCarousel($id: String!) {
-    deleteCarousel(id: $id){
-        ok,
-        message,
-        error
+  mutation deleteCarousel($id: String!) {
+    deleteCarousel(id: $id) {
+      ok
+      message
+      error
     }
   }
 `;
 
 export const GET_CAROUSELS_MUTATION = gql`
-{
-    getCarousels{
-        ok,
-        data{
-            id,
-            title,
-            subtitle,
-            bannerLink,
-            status,
-            imagePath
-            }
-            ,
-        error
+  {
+    getCarousels {
+      ok
+      data {
+        id
+        title
+        subtitle
+        bannerLink
+        status
+        imagePath
+      }
+      error
     }
   }
 `;
 export const GET_CAROUSEL_MUTATION = gql`
-query getCarousel($id: String!){
-    getOneCarousel(id:$id){
-        ok,
-        data{
-            id,
-            title,
-            subtitle,
-            bannerLink,
-            status,
-            imagePath
-            }
-            ,
-        error
+  query getCarousel($id: String!) {
+    getOneCarousel(id: $id) {
+      ok
+      data {
+        id
+        title
+        subtitle
+        bannerLink
+        status
+        imagePath
+      }
+      error
+    }
+  }
+`;
+
+export const GET_COMMISSION_SETTING_MUTATION = gql`
+  {
+    getCommission {
+      ok
+      data {
+        id
+        commission
+      }
+      message
+      error
     }
   }
 `;
