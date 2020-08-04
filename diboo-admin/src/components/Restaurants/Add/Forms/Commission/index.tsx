eff780f8b52e5100604f030ae7f2c977693d100d
Yassine JOUT <yassinejout@gmail.com>
2020-08-04 12:38:44 +0100

    refactor(Restaurants): House keeping

--- diboo-admin/src/components/Restaurants/Add/Forms/Commission/index.tsx
+++ diboo-admin/src/components/Restaurants/Add/Forms/Commission/index.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 import { Field } from "formik";
 
-class CommissionForm extends React.Component<{
+interface Props {
   commission: Number;
   editValues: any;
-}> {
-  constructor(props: any) {
-    super(props);
-  }
-  render = () => {
-    return (
-      <div className="form-group">
-        <label>Commission %</label>
-        <Field
-          type="number"
-          className="form-control"
-          name="commission"
-          style={{ width: 300 }}
-          placeholder={this.props.editValues.commission}
-        />
-      </div>
-    );
-  };
 }
+
+const CommissionForm: React.FunctionComponent<Props> = (props) => {
+  return (
+    <div className="form-group">
+      <label>Commission %</label>
+      <Field
+        type="number"
+        className="form-control"
+        name="commission"
+        style={{ width: 300 }}
+        placeholder={props.editValues.commission}
+      />
+    </div>
+  );
+};
 export default CommissionForm;
