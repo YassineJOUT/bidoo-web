724d3817c8dde3d0378004b4b32c46b9d94c3739
Yassine JOUT <yassinejout@gmail.com>
2020-08-04 13:04:52 +0100

    fix(SideBar): hide side bar on mobile view

--- diboo-admin/src/components/Shared/SideBar/index.tsx
+++ diboo-admin/src/components/Shared/SideBar/index.tsx
@@ -1,38 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import MetisMenu from "react-metismenu";
 import { menuContent } from "../../../utilities/Consts";
+import { useMediaQuery } from "react-responsive";
 interface Props {
-  collapse: boolean
+  collapse: boolean;
 }
 
-class SideBar extends Component<Props> {
+const SideBar: React.FunctionComponent<Props> = () => {
   //history.push("forgotpassword")
-
-  render = () => {
-    
-    
-    return (
-      <div className="left side-menu" >
-        <div className="slimScrollDiv" >
-          <div className="slimscroll-menu mm-show" id="remove-scroll">
-            <div id="sidebar-menu" className="mm-active">
-              <MetisMenu
-                className="sidebar-menu"
-                classNameContainer="metismenu"
-                classNameItemActive="mm-active"
-                classNameLinkActive="mm-active"
-                content={menuContent}
-                activeLinkFromLocation
-              />
-            </div>
-
-            <div className="clearfix"></div>
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 });
+  console.log("isTabletOrMobile");
+  console.log(isTabletOrMobile);
+  return (
+    <div className={isTabletOrMobile ? "left side-menu d-none" : "left side-menu" }>
+      <div className="slimScrollDiv">
+        <div className="slimscroll-menu mm-show" id="remove-scroll">
+          <div id="sidebar-menu" className="mm-active">
+            <MetisMenu
+              className="sidebar-menu"
+              classNameContainer="metismenu"
+              classNameItemActive="mm-active"
+              classNameLinkActive="mm-active"
+              content={menuContent}
+              activeLinkFromLocation
+            />
           </div>
-          <div></div>
+
+          <div className="clearfix"></div>
         </div>
+        <div></div>
       </div>
-    );
-  };
-}
+    </div>
+  );
+};
 
 export default SideBar;
